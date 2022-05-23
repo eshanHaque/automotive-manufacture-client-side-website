98db5a5fd17c66eee33dc1279555a6b829bc1ec7
eshanHaque <eshan.haque16@gmail.com>
2022-05-23 21:35:19 +0600

    banner updated

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,36 +1,17 @@
 import React from 'react';
+import img1 from '../../Images/caros1.jpg';
+import img2 from '../../Images/caros2.jpg';
+
 
 const Banner = () => {
     return (
         <div>
-            <div class="carousel w-full">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img src="" class="w-full"/> 
-                        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide4" class="btn btn-circle">❮</a>
-                            <a href="#slide2" class="btn btn-circle">❯</a>
-                        </div>
+            <div className="carousel carousel-center rounded-box">
+                <div className="carousel-item">
+                    <img src={img1} alt="Factory" />
                 </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img src="" class="w-full"/> 
-                        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide1" class="btn btn-circle">❮</a>
-                            <a href="#slide3" class="btn btn-circle">❯</a>
-                        </div>
-                </div>
-                <div id="slide3" class="carousel-item relative w-full">
-                    <img src="" class="w-full"/> 
-                        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide2" class="btn btn-circle">❮</a>
-                            <a href="#slide4" class="btn btn-circle">❯</a>
-                        </div>
-                </div>
-                <div id="slide4" class="carousel-item relative w-full">
-                    <img src="" class="w-full"/> 
-                        <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide3" class="btn btn-circle">❮</a>
-                            <a href="#slide1" class="btn btn-circle">❯</a>
-                        </div>
+                <div className="carousel-item">
+                    <img src={img2} alt="Factory" />
                 </div>
             </div>
         </div>
